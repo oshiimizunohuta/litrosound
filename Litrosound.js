e6191add150c6764c14079ba625580aad55622ab
oshiimizunohuta <g.ebodai@gmail.com>
2018-04-27 08:30:44 +0900

    chrome66対応

--- Litrosound.js
+++ Litrosound.js
@@ -2,9 +2,9 @@
  * Litro Sound Library
  * Since 2013-11-19 07:43:37
  * @author しふたろう
- * ver 0.11.10
+ * ver 0.11.11
  */
-var LITROSOUND_VERSION = '0.11.10';
+var LITROSOUND_VERSION = '0.11.11';
 
 // var SAMPLE_RATE = 24000;
 // var SAMPLE_RATE = 48000;
@@ -1096,7 +1096,7 @@ LitroPlayer.prototype = {
 	play: function()
 	{
 //		this.onFadeOutFinish();
-		
+		this.litroSound.context.resume();
 		this.systemTime = performance.now();
 		this.playSoundFlag = true;
 		this.delayEventset = makeEventsetData();
